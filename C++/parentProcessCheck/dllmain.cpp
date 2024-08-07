44e4f5be365c6ff2d96b304283d8ffae7cab9b09
XUANXUQAQ <xuanxu233@outlook.com>
2024-08-07 22:54:17 +0800

    提取出获取父进程pid函数

--- C++/parentProcessCheck/dllmain.cpp
+++ C++/parentProcessCheck/dllmain.cpp
@@ -6,6 +6,8 @@
 #include <tlhelp32.h>
 #include <set>
 
+DWORD get_parent_process_id();
+
 BOOL APIENTRY DllMain(HMODULE hModule,
                       DWORD ul_reason_for_call,
                       LPVOID lpReserved
@@ -25,11 +27,32 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 JNIEXPORT jboolean JNICALL Java_file_engine_dllInterface_ParentProcessCheck_isParentProcessExist
 (JNIEnv*, jobject)
 {
+    const auto&& ppid = get_parent_process_id();
+    if (ppid == 0)
+    {
+        return false;
+    }
+    const auto&& p_handle = OpenProcess(PROCESS_QUERY_INFORMATION, false, ppid);
+    if (p_handle == nullptr)
+    {
+        return false;
+    }
+    DWORD exit_code{};
+    // check for status
+    const bool still_exist = GetExitCodeProcess(p_handle, &exit_code) &&
+        exit_code == STILL_ACTIVE;
+    CloseHandle(p_handle);
+    return still_exist;
+}
+
+DWORD get_parent_process_id()
+{
     const HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 pe{};
     pe.dwSize = sizeof(PROCESSENTRY32);
 
     const auto pid = GetCurrentProcessId();
+    DWORD ppid = 0;
 
     if (Process32First(h, &pe))
     {
@@ -37,22 +60,12 @@ JNIEXPORT jboolean JNICALL Java_file_engine_dllInterface_ParentProcessCheck_isPa
         {
             if (pe.th32ProcessID == pid)
             {
-                const auto ppid = pe.th32ParentProcessID;
-                // get process handle
-                const auto&& p_handle = OpenProcess(PROCESS_QUERY_INFORMATION, false, ppid);
-                if (p_handle == nullptr)
-                {
-                    return false;
-                }
-                DWORD exit_code{};
-                // check for status
-                const bool still_exist = GetExitCodeProcess(p_handle, &exit_code) &&
-                    exit_code == STILL_ACTIVE;
-                return still_exist;
+                ppid = pe.th32ParentProcessID;
+                break;
             }
         }
         while (Process32Next(h, &pe));
     }
     CloseHandle(h);
-    return false;
+    return ppid;
 }
